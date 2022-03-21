d00891948b1c46f53b9cc58e517ef163ef500bd0
Yassir <yassir.akram@hotmail.fr>
2022-03-21 02:48:22 +0100

    saving

--- website/__init__.py
+++ website/__init__.py
@@ -7,68 +7,6 @@ from flask_socketio import SocketIO
 
 
 
-def init_player(ID, prenom, mdp, color1, color2):
-    player = {}
-    player["ID"] = ID
-    player["name"] = prenom
-    player["password"] = mdp
-    player["flouze"] = 0
-    player["saved_flouze"] = 0 # Dans le jeu 3 l'argent est mis de coté
-    player["stars"] = 0
-    player["color"] = color1
-    player["secColor"] = color2
-    player["choix"] = None
-    player["done"] = False # Indique si le joueur a fait son choix
-    player["namespace"] = None
-    player["gain_a_partager"] = 0 # Quantitée a partager dans 'partager.htlm'
-    return player
-
-pages = [      # Liste des pages a afficher dans l'ordre / round est de la forme [jeu, manche]
-    { "url": "title.html",       "round": [1, 0], "background": "10.jpg"}, 
-    { "url": "Jeu1-choix.html",  "round": [1, 1], "background": "10.jpg", "prize": 200}, 
-    { "url": "results.html",     "round": [1, 1], "background": "10.jpg"}, 
-    { "url": "Jeu1-choix.html",  "round": [1, 2], "background": "10.jpg", "prize": 400}, 
-    { "url": "results.html",     "round": [1, 2], "background": "10.jpg"}, 
-    { "url": "Jeu1-choix.html",  "round": [1, 3], "background": "10.jpg", "prize": 600, "stars": 1}, 
-    { "url": "results.html",     "round": [1, 3], "background": "10.jpg"}, 
-    { "url": "title.html",       "round": [2, 0], "background": "9.jpg"}, 
-    { "url": "Jeu2-choix.html",  "round": [2, 1], "background": "9.jpg", "prize": 50}, 
-    { "url": "Jeu2-reveal.html", "round": [2, 1], "background": "9.jpg"}, 
-    { "url": "results.html",     "round": [2, 1], "background": "9.jpg"}, 
-    { "url": "Jeu2-choix.html",  "round": [2, 2], "background": "9.jpg", "prize": 100}, 
-    { "url": "Jeu2-reveal.html", "round": [2, 2], "background": "9.jpg"}, 
-    { "url": "results.html",     "round": [2, 2], "background": "9.jpg"}, 
-    { "url": "Jeu2-choix.html",  "round": [2, 3], "background": "9.jpg", "prize": 150, "stars": 2}, 
-    { "url": "Jeu2-reveal.html", "round": [2, 3], "background": "9.jpg"}, 
-    { "url": "results.html",     "round": [2, 3], "background": "9.jpg"}, 
-    { "url": "title.html",       "round": [3, 0], "background": "8.jpg", "initial_flouze": 100}, 
-    { "url": "Jeu3-choix.html",  "round": [3, 1], "background": "8.jpg", "gain": 1.2}, 
-    { "url": "results.html",     "round": [3, 1], "background": "8.jpg"}, 
-    { "url": "Jeu3-choix.html",  "round": [3, 2], "background": "8.jpg", "gain": 1.5}, 
-    { "url": "results.html",     "round": [3, 2], "background": "8.jpg"}, 
-    { "url": "Jeu3-choix.html",  "round": [3, 3], "background": "8.jpg", "gain": 2, "stars": 2}, 
-    { "url": "results.html",     "round": [3, 3], "background": "8.jpg"}, 
-    { "url": "title.html",       "round": [4, 0], "background": "6.jpg"}, 
-    { "url": "Jeu4-choix.html",  "round": [4, 1], "background": "6.jpg", "prize": [[150, 100, 50, 0, "star"]]}, 
-    { "url": "Jeu4-reveal.html", "round": [4, 1], "background": "6.jpg", "prize": [[150, 100, 50, 0, "star"]]}, 
-    { "url": "results.html",     "round": [4, 1], "background": "6.jpg"}, 
-    { "url": "Jeu4-choix.html",  "round": [4, 2], "background": "6.jpg", "prize": [[250, 150, 0, -150, "star"], [400, 250, 0, -250, "star"]]}, 
-    { "url": "Jeu4-reveal.html", "round": [4, 2], "background": "6.jpg", "prize": [[250, 150, 0, -150, "star"], [400, 250, 0, -250, "star"]]}, 
-    { "url": "results.html",     "round": [4, 2], "background": "6.jpg"}, 
-    { "url": "Jeu4-choix.html",  "round": [4, 3], "background": "6.jpg", "prize": [[400, 200, -250, "star", "star"], [600, 250, -300, "star", "star"], [1000, 300, -400, "star", "star"]]}, 
-    { "url": "Jeu4-reveal.html", "round": [4, 3], "background": "6.jpg", "prize": [[400, 200, -250, "star", "star"], [600, 250, -300, "star", "star"], [1000, 300, -400, "star", "star"]]}, 
-    { "url": "results.html",     "round": [4, 3], "background": "6.jpg"}, 
-    { "url": "donner_des_etoiles.html", "round": [0, 0], "background": "6.jpg"}, 
-    { "url": "results.html",     "round": [0, 1], "background": "6.jpg"}, 
-    { "url": "title.html",       "round": [5, 0], "background": "7.jpg", "prize": 2500, "bonus": 500}, 
-    { "url": "Jeu 5",            "round": [5, 1], "background": "7.jpg", "phase": "proposition"}, 
-    { "url": "Jeu 5",            "round": [5, 1], "background": "7.jpg", "phase": "validation"}, 
-    { "url": "Jeu 5",            "round": [5, 1], "background": "7.jpg", "phase": "reveal"}, 
-    { "url": "results.html",     "round": [5, 3], "background": "7.jpg"}
-]
-
-pages_by_round = { tuple(page['round']) : page for page in pages }
-
 theme_colors = {
     "10.jpg":["#b65612","#dfaa84"], 
     "9.jpg": ["#017e68","#6ecdbc"], 
@@ -95,11 +33,7 @@ quiz = [
  "________ _______ le ____ ______ à _________ __ Montréal ?"], 
 ]
 
-def load_data():
-    with open("data.pck", 'rb') as file:
-        gameState, players, log = pickle.load(file)
 
-    return gameState, players, log
 
 def init_game():
     players = []
