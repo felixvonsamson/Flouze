9787ad565be25940a19136aa4fce87ded3c701f7
Yassir <yassir.akram@hotmail.fr>
2022-03-28 00:59:22 +0200

    bug fixes

--- website/views.py
+++ website/views.py
@@ -91,119 +91,118 @@ def donner_etoiles():
 def home():
   player = engine.players[session["ID"]]
   game = engine.current_game
+  if request.method == "POST":
+    if "jeu1" in request.form:
+      if not game.is_allowed_to_play(player, 1):
+        return redirect(url_for("views.home"))
+      tickets = request.form.get("tickets")
+      if tickets == None:
+        flash("Veuiller faire un choix !", category="error")
+        return render_template(engine.current_page["url"], engine=engine, player=player)
+      tickets = int(tickets)
+      player.choice = tickets
+      engine.log(f"{player.name} a choisis {tickets} tickets.")
+      player.flash_message(f"Vous avez choisis {tickets} tickets.")
+      player.is_done = True
+      engine.save_data()
 
-  if "jeu1" in request.form:
-    if not game.is_allowed_to_play(player, 1):
-      return redirect(url_for("views.home"))
-    tickets = request.form.get("tickets")
-    if tickets == None:
-      flash("Veuiller faire un choix !", category="error")
-      return render_template(engine.current_page["url"], engine=engine, player=player)
-    tickets = int(tickets)
-    player.choice = tickets
-    engine.log(f"{player.name} a choisis {tickets} tickets.")
-    player.flash_message(f"Vous avez choisis {tickets} tickets.")
-    player.is_done = True
-    engine.save_data()
+    elif "jeu2" in request.form:
+      if not game.is_allowed_to_play(player, 2):
+        return redirect(url_for("views.home"))
+      if "choice" not in request.form:
+        flash("Veuiller choisir un nombre !", category="error")
+        return render_template(engine.current_page["url"], engine=engine, player=player)
+      player.choice = int(request.form["choice"])
+      engine.log(f"{player.name} a choisis le nombre {player.choice}.")
+      player.flash_message(f"Vous avez choisis le nombre {player.choice}.")
+      player.is_done = True
+      engine.save_data()
 
-  elif "jeu2" in request.form:
-    if not game.is_allowed_to_play(player, 2):
-      return redirect(url_for("views.home"))
-    if "choice" not in request.form:
-      flash("Veuiller choisir un nombre !", category="error")
-      return render_template(engine.current_page["url"], engine=engine, player=player)
-    player.choice = int(request.form["choice"])
-    engine.log(f"{player.name} a choisis le nombre {player.choice}.")
-    player.flash_message(f"Vous avez choisis le nombre {player.choice}.")
-    player.is_done = True
-    engine.save_data()
+    elif "jeu3" in request.form:
+      if not game.is_allowed_to_play(player, 3):
+        return redirect(url_for("views.home"))
+      amount = request.form.get("montant")
+      if amount == "":
+        flash("Veuiller indiquer un montant !", category="error")
+        return render_template(engine.current_page["url"], engine=engine, player=player)
+      amount = int(amount)
+      if amount < 0:
+        flash("Le montant à investir ne peut pas être negatif !", category="error")
+        return render_template(engine.current_page["url"], engine=engine, player=player)
+      if amount > player.flouze:
+        flash("Le montant indiqué dépasse votre solde !", category="error")
+        return render_template(engine.current_page["url"], engine=engine, player=player)
+      player.flouze -= amount
+      player.choice = amount
+      engine.log(f"{player.name} a versé {amount} Pièces dans le pot commun")
+      player.flash_message(f"Vous avez versé {amount} {icons['coin']} dans le pot commun")
+      player.is_done = True
+      engine.save_data()
+        
+    elif "jeu4" in request.form:
+      if not game.is_allowed_to_play(player, 4):
+        return redirect(url_for("views.home"))
+      if "choice" not in request.form:
+        flash("Veuiller choisir un prix !", category="error")
+        return render_template(engine.current_page["url"], engine=engine, player=player)
+      player.choice = int(request.form["choice"])
+      prizes = game.current_prizes
+      prize = prizes[player.choice]
+      if prize == "star":
+        engine.log(f"{player.name} a choisis l'etoile.")
+        player.flash_message(f"Vous avez choisis le prix : {icons['star']}")
+      else:
+        engine.log(f"{player.name} a choisis le prix : {prize} Pièces")
+        player.flash_message(f"Vous avez choisis le prix : {prize} {icons['coin']}")
+      player.is_done = True
+      engine.save_data()
+    
+    elif "don_etoiles" in request.form:
+      assert request.form["don_etoiles"] == "terminer"
+      player.is_done = True
+      engine.save_data()
 
-  elif "jeu3" in request.form:
-    if not game.is_allowed_to_play(player, 3):
-      return redirect(url_for("views.home"))
-    amount = request.form.get("montant")
-    if amount == "":
-      flash("Veuiller indiquer un montant !", category="error")
-      return render_template(engine.current_page["url"], engine=engine, player=player)
-    amount = int(amount)
-    if amount < 0:
-      flash("Le montant à investir ne peut pas être negatif !", category="error")
-      return render_template(engine.current_page["url"], engine=engine, player=player)
-    if amount > player.flouze:
-      flash("Le montant indiqué dépasse votre solde !", category="error")
-      return render_template(engine.current_page["url"], engine=engine, player=player)
-    player.flouze -= amount
-    player.choice = amount
-    engine.log(f"{player.name} a versé {amount} Pièces dans le pot commun")
-    player.flash_message(f"Vous avez versé {amount} {icons['coin']} dans le pot commun")
-    player.is_done = True
-    engine.save_data()
+    elif "jeu5" in request.form:
+      assert request.form["jeu5"] in ["proposition", "nouvelle_proposition", "refuser", "accepter", "quiz_reponse"]
       
-  elif "jeu4" in request.form:
-    if not game.is_allowed_to_play(player, 4):
-      return redirect(url_for("views.home"))
-    if "choice" not in request.form:
-      flash("Veuiller choisir un prix !", category="error")
-      return render_template(engine.current_page["url"], engine=engine, player=player)
-    player.choice = int(request.form["choice"])
-    prizes = game.current_prizes
-    prize = prizes[player.choice]
-    if prize == "star":
-      engine.log(f"{player.name} a choisis l'etoile.")
-      player.flash_message(f"Vous avez choisis le prix : {icons['star']}")
-    else:
-      engine.log(f"{player.name} a choisis le prix : {prize} Pièces")
-      player.flash_message(f"Vous avez choisis le prix : {prize} {icons['coin']}")
-    player.is_done = True
-    engine.save_data()
-  
-  elif "don_etoiles" in request.form:
-    assert request.form["don_etoiles"] == "terminer"
-    player.is_done = True
-    engine.save_data()
-
-  elif "jeu5" in request.form:
-    assert request.form["jeu5"] in ["proposition", "nouvelle_proposition", "refuser", "accepter", "quiz_reponse"]
-    
-    if request.form["jeu5"] == "quiz_reponse":
-      answer = request.form.get("réponse")
-      engine.log(f'{player.name} a donner la réponse {answer} au quiz')
-      game.current_answer = answer
-    
-    elif request.form["jeu5"] == "proposition":
-      if not game.is_allowed_to_play(player, 5):
-        return redirect(url_for("views.home"))
-      total = 0
-      for other_player in game.other_players:
-        amount = request.form.get(other_player.name)
-        if amount == "":
-          flash("Veuiller indiquer un montant pour tous les joueurs !", category="error")
+      if request.form["jeu5"] == "quiz_reponse":
+        answer = request.form.get("réponse")
+        engine.log(f'{player.name} a donner la réponse {answer} au quiz')
+        game.current_answer = answer
+      
+      elif request.form["jeu5"] == "proposition":
+        if not game.is_allowed_to_play(player, 5):
+          return redirect(url_for("views.home"))
+        total = 0
+        for other_player in game.other_players:
+          amount = request.form.get(other_player.name)
+          if amount == "":
+            flash("Veuiller indiquer un montant pour tous les joueurs !", category="error")
+            return render_template("Jeu5-proposition.jinja", engine=engine, player=player)
+          amount = int(amount)
+          total += amount
+        if total > player.flouze:
+          flash("Les propositions que vous avez faites dépasse vos moyens !", category="error")
           return render_template("Jeu5-proposition.jinja", engine=engine, player=player)
-        amount = int(amount)
-        total += amount
-      if total > player.flouze:
-        flash("Les propositions que vous avez faites dépasse vos moyens !", category="error")
-        return render_template("Jeu5-proposition.jinja", engine=engine, player=player)
-      for i, other_player in enumerate(game.other_players):
-        amount = int(request.form.get(other_player.name))
-        amount = int(amount)
-        game.current_proposition[i] = amount
-        other_player.message = f"{player.name} vous fait une proposition de {amount} {icons['coin']}"
-      player.is_done = True
-      engine.next_page()
-      engine.save_data()
-    
-    elif request.form["jeu5"] in ["refuser", "accepter"]:
-      if not game.is_allowed_to_play(player, 5):
-        return redirect(url_for("views.home"))
-      player.choice = request.form["jeu5"] == "accepter"
-      player.is_done = True
-      engine.save_data()
-    
-    elif request.form["jeu5"] == "nouvelle_proposition":
-      engine.next_page()
-      engine.save_data()
-
+        for i, other_player in enumerate(game.other_players):
+          amount = int(request.form.get(other_player.name))
+          amount = int(amount)
+          game.current_proposition[i] = amount
+          other_player.message = f"{player.name} vous fait une proposition de {amount} {icons['coin']}"
+        player.is_done = True
+        engine.next_page()
+        engine.save_data()
+      
+      elif request.form["jeu5"] in ["refuser", "accepter"]:
+        if not game.is_allowed_to_play(player, 5):
+          return redirect(url_for("views.home"))
+        player.choice = request.form["jeu5"] == "accepter"
+        player.is_done = True
+        engine.save_data()
+      
+      elif request.form["jeu5"] == "nouvelle_proposition":
+        engine.next_page()
+        engine.save_data()
 
   if engine.current_page["url"] == "Jeu 5":
     if engine.current_page["phase"] == "proposition":
