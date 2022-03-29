551856879dcf6244d6297d9141a077d9ab85f82a
Yassir <yassir.akram@hotmail.fr>
2022-03-29 13:00:38 +0200

    refactoring

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import request, session
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask import Blueprint
 from functools import partial
 from website import engine
