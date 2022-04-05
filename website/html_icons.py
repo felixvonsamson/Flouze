657577e801570b026ddffebd10f79048c6979d0f
Yassir AKRAM <yassir.akram@staff-net-cx-2381.intern.ethz.ch>
2022-04-05 10:31:51 +0200

    styles bugs

--- website/html_icons.py
+++ website/html_icons.py
@@ -1,5 +1,5 @@
 icons = {
-  "coin": '<img src="/static/images/coin.png" class="coin-small" alt="Coin">',
+  "coin": '<img src="/static/images/coin.png" class="coin" alt="Coin">',
   "star": '<i class="fa fa-star"></i>', 
   "sad": '<i class="fa fa-frown-o"></i>'
 }
