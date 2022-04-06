ba911afda21ec11679479b0f3d5602f82155c0a8
Yassir <yassir.akram@hotmail.fr>
2022-04-06 11:38:36 +0200

    changed port of ssl

--- main_ssl.py
+++ main_ssl.py
@@ -7,5 +7,6 @@ socketio.run(app,
              debug=True, 
              log_output=False, 
              host="0.0.0.0", 
+             port=5443, 
              keyfile="server_privatekey.pem", 
              certfile="server_cert.pem")
